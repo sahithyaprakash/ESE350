13b66ff25573464853e48cfea42741766e5b2912
Phillip H Trent III <ptrent@seas.upenn.edu>
2016-02-09 15:08:23 -0500

    Part3
    Working on Part 3

--- Lab3/program.c
+++ Lab3/program.c
@@ -4,51 +4,78 @@
 #include <string.h>
 #include "../Arduino_ATMega/uart.h"
 
-unsigned long half_period = 0;
-unsigned long duration = 0; //ms
-unsigned long overflows = 0;
+/* 
+ * The following code cycles through row values in a 0-hot state manner
+ * and reads the values of the columns when the interrupt is triggered (every 4ms)
+ */
+unsigned int rowPorts[] = {0xFB, 0xF7, 0xEF, 0xDF}; // Values to set PORTC pins 2, 3, 4, 5 to low 
+unsigned int columnPorts[] = {0x04, 0x08, 0x10, 0x20}; // Values to set PORTC pins 2, 3, 4, 5 to low 
 
-ISR(TIMER1_COMPA_vect) {
-	OCR1A += half_period;
-}
+//Index for keeping track of which row is currently set to 0
+unsigned int rowIndex = 0;
 
-ISR(TIMER1_OVF_vect) {
-	overflows ++;
-	if (overflows >= duration / 4.096) {
-		overflows = 0;
-		duration = 0;
+// Note: when index is -1, there is no data.
+int xIndex = -1; //rows
+int yIndex = -1; //columns 
+
+ISR(TIMER2_COMPA_vect) {
+	//TURN LED ON
+	//PORTB = 0x20;
+
+	// switch 0-hot state to current row index
+	PORTC = rowPorts[rowIndex];
+	//update index based on row and column pressed
+	for (int i = 0; i <= 3; i ++) {
+		//if row port is low, then store the value in xIndex and store columnIndex = yIndex
+		if (PIND & 0xFF == 0xFF) {
+			//TURN LED ON
+			PORTB = 0x20;
+			break;
+		} else if (PIND | columnPorts[i] == 0xFF) {
+			printf("w");
+			//store key
+			yIndex = i;
+			xIndex = rowIndex;
+		}
+	}
+
+	//update row index for next time. 
+	if (rowIndex >= 3) {
+		rowIndex = 0;
+	} else {
+		rowIndex ++;
 	}
 }
 
 int main (void) {
 	//initialize convenience variables
-	unsigned long clockFrequency = 16000000; //Hz
-	unsigned long frequency = 0;			//Hz
+	char letters[4][4] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
 
 	//initialize controller state
 	uart_init();
-	DDRB = 0xFF; 					// make all pins outputs
-	TCCR1B = 0x01; 					// starts the timer with no prescaler
-	TIMSK1 = 0x02; 					// unmasks the timer interrupt for compare
-	TIMSK1 |= 0x01;					// unmasks the timer interrupt for overflow
-	
-	//get user input
-	scanf("%lu %lu", &frequency, &duration);
-	printf("%lu %lu", frequency, duration);
 
-	//calculate half period
-	half_period = clockFrequency / frequency / 2;
-	
-	TCCR1A |= 0x40; 				// toggles pin 9 when a compare match is made.
-	OCR1A += TCNT1 + half_period; 	//starts at low and waits until the first half period is reached
-	
+	//Set data direction for Ports C and D
+	DDRC = 0xFF; 					// make all C pins outputs
+	DDRD = 0x00; 					// make all D pins inputs
+	DDRB = 0xFF;					// make the LED an output
+	PIND = 0xFF;					// make all input pins high
+
+	//Setup the timer and CTC
+	TCCR2A = 0x02;					// sets timer to CTC mode
+	OCR2A = 0xFF;					// timer upper limit (resets after this value)
+	TCCR2B = 0x01; 					// starts the timer with no prescaler
+	TIMSK2 = 0x02; 					// unmasks the timer interrupt for compare
+		
 	sei();							//start interrupts
 
 	while (1) {
-		//TODO: check for errors
-		if (frequency == 0 || duration == 0) {
-			TCCR1A = 0x80;
-			continue;
+		//Turn LED OFF
+		PORTB = 0x00;
+		if (xIndex >=  0 || yIndex >= 0) {
+			printf("(%i, %i)", xIndex, yIndex);
+			putchar(letters[xIndex][yIndex]);
+			xIndex = -1;
+			yIndex = -1;
 		}
 	}
 }
--- Tutorial1/tutorial_servo.c
+++ Tutorial1/tutorial_servo.c
@@ -1,37 +1,43 @@
+#include <unistd.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdio.h>
-#include <string.h>
 #include "../Arduino_ATMega/uart.h"
 
-#define MAX_OVERFLOWS 10000
-int i = 0;
-int overflows = 0;
-
-ISR(TIMER1_OVF_vect) {
-	//overflows ++;
-	putchar('o');
-}
+#define OCR0A_VALUE 187//.5 , which is equivalent to a 3ms period
 
 int main (void) {
 	//initialize convenience variables
-	unsigned long clockFrequency = 16000000; //Hz
-	unsigned long frequency = 0;			//Hz
+	// unsigned long clockFrequency = 16000000; //Hz
+	// unsigned long frequency = 0;			//Hz
 
 	//initialize controller state
 	uart_init();
-	DDRB = 0xFF; 					// make all pins outputs
-	TCCR1B = 0x05; 					//start timer, prescaler = 1024
-	TIMSK1 = 0x01; 					//enable overflow interrupt
-	sei();
-	PORTB = 0xFF;
+	// DDRB = 0xFF; 					// make all pins outputs
+	TCCR0B = 0x04; 						//start timer, prescaler = 256
+	// TIMSK1 = 0x01; 					//enable overflow interrupt
+	// sei();
 
- //    PORTB = 0x04; //turn port 10 to high voltage
- //    PORTB = 0x08; //turn port 11 to high voltage
+	DDRD |= 0x40; // set OC0A as output
+	TCCR0A |= 0x80; // use non-inverting output
+	TCCR0A |= 0x03; // use fast PWM mode
+	TCCR0B |= 0x01; // turn the timer on
+
+	OCR0A = 0xFF; // which is the OCR0A_VALUE
+
+	// wait(1);
+
+	// OCR0A = 0xE6; // 90% Duty Cycle
+
+	// sleep(2);
+
+	 OCR0B = 0xB4; // 70% Duty Cycle
+
+	// pause(2);
+
+	while(1) ;
+
+    // PORTB = 0x04; //turn port 10 to high voltage
+ 	// PORTB = 0x08; //turn port 11 to high voltage
 	// PORTB = 0x10; //turn port 12 to high voltage
 	// PORTB = 0x20; //turn port 13 to high voltage
-
-	while (overflows <= 100 * MAX_OVERFLOWS) {
-		//PORTB = 0x04; //turn port 10 to high voltage
-	};
 }
