d1e2443478b1c76571ca0118b10235ecda274ea6
Phillip H Trent III <ptrent@seas.upenn.edu>
2016-02-16 20:13:37 -0500

    Working Towards Working
    Next time, try polling to generate the output.

--- Lab4/program.c
+++ Lab4/program.c
@@ -14,30 +14,12 @@ unsigned int isRisingEdge = TRUE;
 
 
 void setupForSendingPulse() {	
-	printf("sp");
-	cli();
-	TCCR1A = 0x80; 						//Clears OC1A (PB1) on compare match and 
-	TCCR1B = 0x01;						//Stops input capture 
 	DDRB = 0xFF;						//Set Port B to output
 	PORTB = 0x02;						//Set PB1 to high
-	OCR1A = TCNT1 + 80; 				//set to match on 5us
-	TIFR1 = 0x20;						//clears the input capture flag
-	TIMSK1 = 0x01; 						//unmasks interrupt for overflows
-	TIMSK1 |= 0x02; 					//unmasks interrupt for OC1A
-	sei();
 }
 
 void setupForReceivingPulse() {
-	printf("rp");
-	isRisingEdge = TRUE;
 	DDRB = 0x00;						//Set all B pins as inputs
-	TCCR1A = 0x00;						//Disconnect OC
-	TCCR1B |= 0x40;						//Trigger on rising edge
-	TIFR1 = 0x20;						//Clears input capture flag
-	TIMSK1 = 0x20;						//unmasks interrupt for input capture
-	TIMSK1 |= 0x02; 					//unmasks interrupt for OC1A
-	sei();	
-
 }
 
 ISR(TIMER1_OVF_vect) {
@@ -64,35 +46,47 @@ ISR(TIMER1_CAPT_vect) {
 
 		unsigned int diff = endTicks - startTicks;	//Get the difference in the ticks
 		width = (long) overflows * 65535u + (long) diff;	//get the width of the pulse
+
+		isRisingEdge = TRUE;
 	}
 
 
 }
 
 ISR(TIMER1_COMPA_vect) {
-	cli();
 	//sets timer 1 to receive ping
 	setupForReceivingPulse();
+	OCR1A += 80;
 }
 
 int main (void) {
 	//initialize controller state
 	uart_init();
 
-	setupForSendingPulse();
+	//Setup Input Capture
+	TCCR1B |= 0x40;						//Trigger on rising edge
+	TIFR1 = 0x20;						//clears the input capture flag
 
-	//Setup timer 1 to generate ping
+	//Setup Output Compare
+	TCCR1A = 0x80; 						//Clears OC1A (PB1) on compare match
+	OCR1A = TCNT1 + 80;					//set to match on 5us
+
+	//Setup General Clock Functions
+	TCCR1B |= 0x01;						//start clock with no prescaler
+
+	//Unmask Interrupts
 	TIMSK1 = 0x02; 						//unmasks interrupt for OC1A
 	TIMSK1 |= 0x01; 					//unmasks interrupt for overflows
 	TIMSK1 |= 0x20;						//unmasks interrupt for input capture
-	TCCR1B |= 0x01;						//start clock with no prescaler
-	TCCR1A = 0x80; 						//Clears OC1A (PB1) on compare match
+	
+
+	setupForSendingPulse();
 
 	sei();								//start interrupt
 
 	while (1) {
 		if (width != 0) {
-			//printf("%u\n", width);
+			printf("%u\n", width);
 			setupForSendingPulse();
 			width = 0;
 		} 
