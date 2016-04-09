1b5f0ec1bab40f3dc3c230aac4a13c912438ac54
Phillip H Trent III <ptrent@seas.upenn.edu>
2016-04-08 20:00:53 -0400

    Incremental Updates
    Fixed quite a few errors and made things quicker/more reliable. It’s
    still not working, but it’s closer.

--- FinalProject/Arduino/Sensor_Test_1/main.c
+++ FinalProject/Arduino/Sensor_Test_1/main.c
@@ -3,23 +3,22 @@
 #include "../Arduino_ATMega/uart.h"
 
 int main (void) {
-
+	unsigned int last_highest_conductor = 0x00;
 	//initialize controller state
 	uart_init();
-	DDRB = 0xFF; 					// make all pins outputs
-	TCCR0B = 0x05; 					// start timer, prescaler = 256
-
+	TCCR0A = 0x05; 					// start timer, prescaler = 1024
 
 	DDRD = 0xFF; // set DDRD as output
 	DDRB = 0xEF; // set everything but PB4 to outputs
 
 	while(1) {
 		unsigned int highest_conductor = 0x00;
-		for (unsigned int current = 0x00; current <= 0x7F; current ++) {
-			PORTD &= 0x03; 				//clear the current select on D
-			PORTD |= ((current << 2) & 0x03); 		//set the new select
-			PORTB &= 0xFE;				//clear the current value
-			PORTB |= ((current >> 6) & 0x01);		//set the highest bit
+		unsigned int current = 0x00;
+		for (unsigned int current = 0x00; current <= 0x57; current ++) {
+			PORTD = (((current << 2) & 0xFC) | (PORTD & 0x03)); 		//set the new select
+
+			PORTB = (((current >> 6) & 0x01) | (PORTB & 0xFE));		//set the highest bit
+
 			unsigned int a = 0x00;
 
 			while(a < 0xFF) {
@@ -27,10 +26,12 @@ int main (void) {
 			}
 			
 			if ((PINB & 0x10) == 0x10) {
-				printf("current changed to: %i", current);
 				highest_conductor = current;
 			}
 		}
-		printf("Highest Conductor: %i \n", highest_conductor);
+		//if (highest_conductor != last_highest_conductor) {
+		//	last_highest_conductor = highest_conductor;
+			printf("Highest Conductor: %i \n", highest_conductor);
+		//}
 	};
 }
