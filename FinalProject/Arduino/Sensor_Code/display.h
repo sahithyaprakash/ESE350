552e49233514ccdf9c91e2969e226cd5118e45e0
Phillip H Trent III <ptrent@seas.upenn.edu>
2016-04-21 22:01:55 -0400

    Header Files
    Updating header files

---
+++ FinalProject/Arduino/Sensor_Code/display.h
@@ -0,0 +1,13 @@
+
+// does all of the initialization work for the display
+void initializeDisplay();
+
+// 
+void setOutputPerHour(unsigned int beforeDecimalPlace, unsigned int afterDecimalPlace);
+
+// 
+void totalOutput(unsigned int beforeDecimalPlace, unsigned int afterDecimalPlace);
+
+// call when you want the entire display to be updated
+void updateDisplay();
+
--- FinalProject/Arduino/Sensor_Code/storage.h
+++ FinalProject/Arduino/Sensor_Code/storage.h
@@ -1,7 +1,16 @@
+// - - - -  INITIALIZATION - - - -
+
+// Initializes all variables for the storage class
+void initializeStorage(unsigned int frequencyOfCollection);
+
 // - - - - STORAGE - - - -
 
-//
+// Clears all the data currently in storage
 double clearData();
+
+// Takes in the total amount of liquid currently in the container (summed over
+// all columns in mL) and adds the datapoint. 
+double addData(double liquidAmount);
 
 // - - - - MATH - - - -
 
@@ -12,14 +21,13 @@ double volumeFromHighestConductor(unsigned int highestConductor);
 //returns the conductivity in Siemens from the voltage
 double conductivityFromVoltage(unsigned int voltage);
 
-double
-
 // - - - - CALCULATIONS ON DATA - - - -
 
-//
+// returns the total amount of output in mL from the 
+// start of collection to the most recent data point
 double totalOutputFromStartOfCollection();
 
-//
+// returns the total amout of output in mL from just the last hour
 double totalOutputFromTheLastHour();
 
 //
