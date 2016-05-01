b7d6fd5fdad7d52008255b934778179472633330
Phillip H Trent III <ptrent@seas.upenn.edu>
2016-05-01 14:32:58 -0400

    Storage.h
    Changing the header file to store and retrieve more appropriate data.

--- FinalProject/Arduino/Sensor_Code/storage.h
+++ FinalProject/Arduino/Sensor_Code/storage.h
@@ -12,18 +12,22 @@
 
 // - - - -  INITIALIZATION - - - -
 
-// Initializes all variables for the storage class
+// initializes all variables for the storage class
 // param frequencyOfCollection: number of samples taken per hour 
 void initializeStorage(unsigned int frequencyOfCollection);
 
-// - - - - STORAGE - - - -
+// - - - - MANAGE STORAGE - - - -
 
-// Clears all the data currently in storage
+// clears all the data currently in storage
 void clearData();
 
-// Takes in the total amount of liquid currently in the container (summed over
+// takes in the total amount of liquid currently in the container (summed over
 // all columns in mL) and adds the datapoint. 
 float addData(double liquidAmount);
+
+// called when the more specific data should be compressed into a bundle. For the current
+// implementation, this will be called once at every hour.
+void archiveData();
 
 // - - - - MATH - - - -
 
@@ -34,15 +38,17 @@ float volumeFromHighestConductor(unsigned int highestConductor);
 //returns the conductivity in Siemens from the voltage
 float conductivityFromVoltage(unsigned int voltage);
 
-// - - - - CALCULATIONS ON DATA - - - -
+// - - - - RETRIEVE DATA - - - -
 
-// returns the total amount of output in mL from the 
-// start of collection to the most recent data point
-float totalOutputFromStartOfCollection();
+// returns the total amount of liquid currently in the container in mL. For example, 
+// if the patient was admitted at 7:00, the container was emptied at 8:00, then it
+// should return the amount of liquid since 8:00.
+float currentLiquidLevel();
 
 // returns the total amout of output in mL from just the last hour
 float totalOutputFromTheLastHour();
 
-//
-float averageOutputFromStartOfCollection();
-
+// returns the total amount of liquid added to the container since 
+// the last hour. For example, if it's 7:15, then this function should
+// return how much liquid has been added since 7:00. 
+float totalOutputSinceTheLastHour();
